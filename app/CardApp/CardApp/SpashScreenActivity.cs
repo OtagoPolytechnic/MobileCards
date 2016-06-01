6713d4996b41228dadccfaefd07a7f0fa0b6151c
iiSABRE <cameronthomasjackson@gmail.com>
2016-06-01 16:18:32 +1200

    Fixed Merge Problems

--- app/CardApp/CardApp/SpashScreenActivity.cs
+++ app/CardApp/CardApp/SpashScreenActivity.cs
@@ -15,6 +15,7 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using System.Threading;
 
 namespace CardApp
 {
@@ -26,7 +27,17 @@ namespace CardApp
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.Splash_Screen_Layout);
 
-            RunAsync().Wait();           
+            var progressDialog = ProgressDialog.Show(this, "Please wait...", "Checking server info...", true);
+            new Thread(new ThreadStart(delegate
+            {
+                RunAsync().Wait();
+                RunOnUiThread(() => Toast.MakeText(this, "Updateing Files", ToastLength.Long).Show());
+                
+                RunOnUiThread(() => progressDialog.Hide());
+            })).Start();
+
+
+                       
         }
 
 
@@ -34,33 +45,20 @@ namespace CardApp
         {
             using (var client = new HttpClient())
             {
-                client.BaseAddress = new Uri("http://localhost:9000/");
+                //To be added - Base address
+                client.BaseAddress = new Uri("");
                 client.DefaultRequestHeaders.Accept.Clear();
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
                 // HTTP GET
-                HttpResponseMessage response = await client.GetAsync("api/products/1");
+                HttpResponseMessage response = await client.GetAsync("api/words/1");
                 if (response.IsSuccessStatusCode)
                 {
-                    Product product = await response.Content.ReadAsAsync<Product>();
-                    Console.WriteLine("{0}\t${1}\t{2}", product.Name, product.Price, product.Category);
+                    JsonArrayAttribute newDelEditData = await response.Content.ReadAsAsync<JsonArrayAttribute>();
+                    
                 }
 
-                // HTTP POST
-                var gizmo = new Product() { Name = "Gizmo", Price = 100, Category = "Widget" };
-                response = await client.PostAsJsonAsync("api/products", gizmo);
-                if (response.IsSuccessStatusCode)
-                {
-                    Uri gizmoUrl = response.Headers.Location;
-
-                    // HTTP PUT
-                    gizmo.Price = 80;   // Update price
-                    response = await client.PutAsJsonAsync(gizmoUrl, gizmo);
-
-                    // HTTP DELETE
-                    response = await client.DeleteAsync(gizmoUrl);
-                }
-            }
+           
         }
 
     }
